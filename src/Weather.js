ac11be22723c99eaa89134b47636c4017309f560
IngaMoss <83215456+IngaMoss@users.noreply.github.com>
2021-08-11 11:29:00 +0200

    added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -4,20 +4,19 @@ import './Weather.css';
 export default function Weather() {
     return (
         <div className="Weather">
-
-            <form id="search-form" >
+            <form>
                 <div className="row">
-                    <div className="col-7">
-                        <input type="search" placeholder="Enter a city.." className="form-control" />
+                    <div className="col-9">
+                        <input type="search" placeholder="Enter a city.." className="form-control" autoFocus="on" />
                     </div>
                     <div className="col-3">
-                        <input type="Submit" value="    Search    " className="btb btn-primary" />
+                        <input type="Submit" value="Search" className="btn w-100" />
                     </div>
                 </div>
             </form >
 
             <div className="row">
-                <div className="col-3">
+                <div className="col-4">
                     <h1>Lisbon</h1>
                     <ul>
                         <li>Tuesday 13:20</li>
@@ -27,10 +26,9 @@ export default function Weather() {
                     </ul>
                 </div>
                 <div className="col-4">
-                    <img src="" alt="=)" /> 6°C
-                </div>
+                    <img src="http://openweathermap.org/img/wn/02d@2x.png" alt="=)" /><span className="Temperature">6</span><span className="Unit">°C</span></div>
                 <div className="col-4">
-                    <input type="Submit" value="My location" className="btb btn-primary" />
+                    <button type="Submit" className="Location">Location</button>
                 </div>
             </div>
         </div>
