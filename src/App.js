205f2d1693355adfc40eb636dd7d0940b9506a3f
IngaMoss <83215456+IngaMoss@users.noreply.github.com>
2021-08-22 11:57:57 +0200

    added API integration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="New York" />
         <footer>
           <a href="https://github.com/IngaMoss/weather-app-react" target="_blank" rel="noreferrer">Open-source
             code</a>, created by Inga Mosienko
--- src/Weather.js
+++ src/Weather.js
@@ -1,36 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import './Weather.css';
 
-export default function Weather() {
-    return (
-        <div className="Weather">
-            <form>
-                <div className="row">
-                    <div className="col-9">
-                        <input type="search" placeholder="Enter a city.." className="form-control" autoFocus="on" />
-                    </div>
-                    <div className="col-3">
-                        <input type="Submit" value="Search" className="btn w-100" />
-                    </div>
-                </div>
-            </form >
+export default function Weather(props) {
+    const [weatherData, setWeatherData] = useState({ ready: false });
+    function handleResponse(response) {
+        console.log(response.data)
+        setWeatherData({
+            ready: true,
+            city: response.data.name,
+            date: "",
+            iconUrl: "",
+            temperature: response.data.main.temp,
+            description: response.data.weather[0].main,
+            wind: response.data.wind.speed,
+            humidity: response.data.main.humidity
 
-            <div className="row">
-                <div className="col-4">
-                    <h1>Lisbon</h1>
-                    <ul>
-                        <li>Tuesday 13:20</li>
-                        <li>Clear sky</li>
-                        <li>Humidity: 57%</li>
-                        <li>Wind: 2 km/h</li>
-                    </ul>
-                </div>
-                <div className="col-4">
-                    <img src="http://openweathermap.org/img/wn/02d@2x.png" alt="=)" /><span className="Temperature">6</span><span className="Unit">°C</span></div>
-                <div className="col-4">
-                    <button type="Submit" className="Location">Location</button>
+        });
+    }
+    if (weatherData.ready) {
+        return (
+            <div className="Weather">
+                <form>
+                    <div className="row">
+                        <div className="col-9">
+                            <input type="search" placeholder="Enter a city.." className="form-control" autoFocus="on" />
+                        </div>
+                        <div className="col-3">
+                            <input type="Submit" value="Search" className="btn w-100" />
+                        </div>
+                    </div>
+                </form >
+
+                <div className="row">
+                    <div className="col-4">
+                        <h1>{weatherData.city}</h1>
+                        <ul>
+                            <li>{weatherData.date}</li>
+                            <li className="text-capitalize">{weatherData.description}</li>
+                            <li>Humidity: {weatherData.humidity}%</li>
+                            <li>Wind: {weatherData.wind} km/h</li>
+                        </ul>
+                    </div>
+                    <div className="col-4">
+                        <img src={weatherData.iconUrl} alt={weatherData.description} /><span className="Temperature">{Math.round(weatherData.temperature)}</span><span className="Unit">°C</span></div>
+                    <div className="col-4">
+                        <button type="Submit" className="Location">Location</button>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
-};
+        );
+    } else {
+        const apiKey = "6a4eedc94ba63bc335cf63285a0c725f";
+        let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(handleResponse);
+
+        return "Loading...";
+    }
+}
